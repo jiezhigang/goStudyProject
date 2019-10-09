11e063428c168ddb279de895640f98e0141920b9
jiezhigang <jiezhigang@qianxin.com>
2019-10-09 17:12:24 +0800

    update 子测试/hello参数为空时显示hello, world

--- hello_world/Hello.go
+++ hello_world/Hello.go
@@ -5,6 +5,9 @@ import "fmt"
 const helloPrefix = "hello, "
 
 func Hello(name string) string{
+	if name == "" {
+		name = "world"
+	}
 	return helloPrefix + name
 }
 
--- hello_world/hello_world_test.go
+++ hello_world/hello_world_test.go
@@ -3,19 +3,25 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("world")
-	want := "hello, world"
 
-	if got != want {
-		t.Errorf("got '%s' want '%s'", got, want)
+	assertCorrectMessage := func(t *testing.T, got string, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got '%s' want '%s'", got, want)
+		}
 	}
-}
+	t.Run("say hello, george", func(t *testing.T) {
+		got := Hello("george")
+		want := "hello, george"
 
-func TestHello2(t *testing.T) {
-	got := Hello("george")
-	want := "hello, george"
+		assertCorrectMessage(t, got, want)
+	})
 
-	if got != want {
-		t.Errorf("got '%s' want '%s'", got, want)
-	}
+	t.Run("say hell world when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "hello, world"
+
+		assertCorrectMessage(t, got, want)
+	})
+
 }
